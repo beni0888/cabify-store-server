4429faedaf4c98d57e71ca3cef13c49d9e808c41
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2019-04-01 09:56:05 +0200

    Refactor ErrorHandler

--- infrastructure-rest/src/main/java/com/jbenitoc/infrastructure/rest/ErrorHandler.java
+++ infrastructure-rest/src/main/java/com/jbenitoc/infrastructure/rest/ErrorHandler.java
@@ -3,20 +3,20 @@ package com.jbenitoc.infrastructure.rest;
 import com.jbenitoc.domain.store.CartDoesNotExist;
 import com.jbenitoc.domain.store.ItemDoesNotExist;
 import com.jbenitoc.infrastructure.rest.dto.ErrorResponse;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({CartDoesNotExist.class, ItemDoesNotExist.class})
-    public ResponseEntity<Object> handleNotFoundError(RuntimeException ex, WebRequest request) {
-        ErrorResponse response = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
-        return handleExceptionInternal(ex, response, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResponse handleNotFoundError(RuntimeException ex) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
     }
 }
