ae170be3e6d4f413e5a8531d6683bbd436de1754
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2019-04-02 10:13:42 +0200

    Rename CabifyServerApplication to CabifyStoreApplication

--- infrastructure-rest/src/main/java/com/jbenitoc/infrastructure/CabifyStoreApplication.java
+++ infrastructure-rest/src/main/java/com/jbenitoc/infrastructure/CabifyStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CabifyServerApplication {
+public class CabifyStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CabifyServerApplication.class, args);
+        SpringApplication.run(CabifyStoreApplication.class, args);
     }
 
 }
