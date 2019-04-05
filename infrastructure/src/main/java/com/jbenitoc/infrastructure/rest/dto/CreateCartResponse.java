5920ed3312f0648e1f251f34f962a74659dae1f1
Jesús Miguel Benito Calzada <beni0888@hotmail.com>
2019-04-05 09:38:11 +0200

    Refactor CreateCartResponse to keep homogeneity

--- infrastructure/src/main/java/com/jbenitoc/infrastructure/rest/dto/CreateCartResponse.java
+++ infrastructure/src/main/java/com/jbenitoc/infrastructure/rest/dto/CreateCartResponse.java
@@ -2,12 +2,10 @@ package com.jbenitoc.infrastructure.rest.dto;
 
 import com.jbenitoc.domain.store.Cart;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
-@Getter
 public final class CreateCartResponse {
-    private final String id;
+    public final String id;
 
     public static CreateCartResponse fromCart(Cart cart) {
         return new CreateCartResponse(cart.getId().getValue().toString());
